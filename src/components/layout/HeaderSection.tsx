919d86ce05350bdaa91aa2160ed561b85e7c571e
Kais Sraieb <kais@gmail.com>
2025-06-28 02:36:07 +0100

    feat: enhance HeaderSection with email copy functionality and update TopHeader to display current time with GMT offset

--- src/components/layout/HeaderSection.tsx
+++ src/components/layout/HeaderSection.tsx
@@ -1,12 +1,23 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { Copy, MapPin } from "lucide-react";
+import { Copy, MapPin, Check } from "lucide-react";
 import { Button } from "../ui/button";
+import { toast } from "sonner";
+import { useState } from "react";
 
 export function HeaderSection() {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = () => {
+    setCopied(true);
+    navigator.clipboard.writeText("kaissraieb@gmail.com");
+    toast.success("Email copied to clipboard !");
+    setTimeout(() => setCopied(false), 2000);
+  };
+
   return (
     <section className="px-6 py-12 max-w-4xl mx-auto">
-      <div className="flex items-start gap-6 mb-8">
-        <Avatar className="w-16 h-16">
+      <div className="flex items-center sm:items-start gap-6 mb-8 flex-col sm:flex-row">
+        <Avatar className="w-50 h-50 sm:w-16 sm:h-16">
           <AvatarImage
             src="/kais.png"
             className="rounded-full"
@@ -15,18 +26,20 @@ export function HeaderSection() {
           <AvatarFallback>KS</AvatarFallback>
         </Avatar>
         <div>
-          <div className="flex items-center gap-2 mb-1">
-            <h1 className="text-xl font-semibold">KAIS SRAIEB</h1>
-            <div className="w-2 h-2 bg-blue-500 rounded-full"></div>
+          <div className="flex items-center sm:justify-start justify-center gap-2 mb-1 ">
+            <h1 className="text-2xl font-bold">KAIS SRAIEB</h1>
+            <div className="w-2 h-2 bg-blue-500 rounded-full animate-pulse"></div>
           </div>
-          <p className="text-gray-400 mb-4">Full-Stack Web Developer</p>
-          <div className="flex items-center gap-4 text-sm text-gray-400 mb-4">
-            <div className="flex items-center gap-1">
+          <p className="text-gray-400 mb-4 sm:text-start text-center">
+            Full-Stack Web Developer
+          </p>
+          <div className="flex items-center gap-4 text-sm text-gray-400 mb-4 sm:justify-start justify-center">
+            <div className="flex items-center gap-1 ">
               <MapPin className="w-4 h-4" />
               Based in Zarzis, Tunisia 🇹🇳
             </div>
           </div>
-          <p className="text-gray-300 mb-4 max-w-2xl">
+          <p className="text-gray-300 mb-6 max-w-2xl flex-wrap sm:text-start text-center">
             I'm Kais Sraieb, a Full-Stack Web Developer 🧠💻 I build powerful,
             user-friendly web apps with React.js on the front and Python
             (FastAPI/Flask) on the back. Based in Tunisia 🇹🇳, I blend clean
@@ -36,13 +49,13 @@ export function HeaderSection() {
             <br />
             Always learning, always building, always pushing the web forward 🚀
           </p>
-          <p className="text-sm text-gray-400 flex items-center gap-1">
+          <p className="text-sm text-gray-400 flex items-center gap-1 sm:justify-start justify-center">
             <Button
-              // onClick={copyToClipboard}
-              className="ml-1 text-gray-300 hover:text-white transition cursor-pointer"
+              onClick={copyToClipboard}
+              className="ml-1 text-gray-300 hover:text-white transition cursor-pointer flex items-center gap-2"
             >
-              <Copy size={20} />
-              Copy Email
+              {copied ? <Check size={20} /> : <Copy size={20} />}
+              {copied ? "Copied" : "Copy Email"}
             </Button>
           </p>
         </div>
--- src/components/layout/TopHeader.tsx
+++ src/components/layout/TopHeader.tsx
@@ -1,13 +1,36 @@
 import { Clock } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export function TopHeader() {
+  const getDateWithGMTOffset = () => {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+
+    // get GMT Offset in hours
+    const timeZoneOffsetMin = now.getTimezoneOffset(); // in minutes
+    const offsetHours = -timeZoneOffsetMin / 60;
+    const gmt = `GMT${offsetHours >= 0 ? "+" : ""}${offsetHours}`;
+
+    return `${hours}:${minutes}:${seconds} ${gmt}`;
+  };
+
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    setInterval(() => {
+      setDate(getDateWithGMTOffset().toString());
+    });
+  }, []);
+
   return (
     <header className="flex justify-between items-center p-6 border-b border-gray-800">
       <div className="text-sm text-gray-400">EST • 2022</div>
       <div className="flex items-center gap-4 text-sm text-gray-400">
         <div className="flex items-center gap-1">
           <Clock className="w-4 h-4" />
-          13:14:28 GMT+1
+          {date}
         </div>
       </div>
     </header>
